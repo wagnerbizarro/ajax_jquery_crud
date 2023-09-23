35c11ef985f37651b3fdd27e58337a202b7e5c4e
wagnerbizarro <wagnerbzr@gmail.com>
2023-09-23 18:19:26 -0300

    Feat: start php!

---
+++ public/assets/php/app.php
@@ -0,0 +1,8 @@
+<?php
+require 'connection.php';
+
+
+if(isset($POST['read'])){
+    sleep(2);
+    echo "CHEQUEI AQUI!<br>";
+}
---
+++ public/assets/php/connection.php
@@ -0,0 +1,34 @@
+<?php
+
+//DATABASE
+$db = new SQLite3('../db/db1.db');
+
+function create($db)
+{
+}
+
+function read($db)
+{
+    $result = $db->query('SELECT rowid, * FROM users');
+
+    $users = [];
+
+    while ($data = $result->fetchArray(SQLITE3_ASSOC)) {
+        array_push($users, $data);
+    }
+
+    foreach ($users as $key => $users) {
+        echo $users['firstName'] . ' ' . $users['lastName'] . ' ' . $users['age'];
+    }
+}
+
+function update($db)
+{
+}
+
+function delete($db)
+{
+}
+
+
+//read($db);
