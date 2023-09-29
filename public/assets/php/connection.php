580a8322e110f4bfc88ef184b5d3cea8c319cc9b
wagnerbizarro <wagnerbzr@gmail.com>
2023-09-28 23:55:16 -0300

    Feat: Falta o Update !

--- public/assets/php/connection.php
+++ public/assets/php/connection.php
@@ -3,16 +3,8 @@
 //DATABASE
 $db = new SQLite3('../db/db1.db');
 
-//CREATE
-function create($db, $firstName, $lastName, $age)
-{
-    $db->exec("INSERT INTO users(firstName, lastName, age) VALUES('$firstName','$lastName','$age')");
-    echo "Cadastro realizado com sucesso !";
-}
-
 //READ
-function read($db, $name)
-{
+function read($db, $name){
     if ($name == NULL) {
         $result = $db->query('SELECT rowid, * FROM users');
     } else {
@@ -41,27 +33,38 @@ function read($db, $name)
 
     foreach ($users as $key => $users) {
         //echo $users['firstName'] . ' ' . $users['lastName'] . ' ' . $users['age'] . '<br>';
+        $id = $users['rowid'];
+        $firstName = $users['firstName'];
+        $lastName = $users['lastName'];
+        $age =$users['age'];
+
         echo '<tr>';
-        echo '<td>'.$users['rowid'].'</td>';
-        echo '<td>'.$users['firstName'].'</td>';
-        echo '<td>'.$users['lastName'].'</td>';
-        echo '<td>'.$users['age'].'</td>';
-        echo '<td><a href="update.html"">Edit</a></td>';
+        echo '<td>'.$id.'</td>';
+        echo '<td>'.$firstName.'</td>';
+        echo '<td>'.$lastName.'</td>';
+        echo '<td>'.$age.'</td>';
+        echo '<td><a href="#" onclick="prepareUpdate('.$id.');">Edit</a></td>';
         echo '<td><a href="#" onclick="Delete('.$users['rowid'].');">Deletar</a></td>';
         echo '</tr>';
 
     }
 }
 
-//UPDATE
-function update($db,$id)
-{
-    echo "UPDATEEEEE";
+//CREATE
+function create($db, $firstName, $lastName, $age){
+    $db->exec("INSERT INTO users(firstName, lastName, age) VALUES('$firstName','$lastName','$age')");
 }
 
 //DELETE
-function delete($db,$id)
-{
+function delete($db,$id){
     $db->exec("DELETE FROM users WHERE rowid = $id");
-    echo "Cadastro deletado com sucesso !";
 }
+
+//UPDATE
+function update($db,$id,$firstName,$lastName,$age){
+    $result = $db->query('SELECT rowid, * FROM users');
+    echo "UPDATEEEEE";
+    echo "$id,$firstName, $lastName,$age";
+}
+
+
